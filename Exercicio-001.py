1c8b52c71c3e50118fa90299b71eb458920667e7
Lucass_Santana <luucasdsp@hotmail.com>
2025-03-22 09:35:37 -0300

    Plot o ponto de interseção

--- Exercicio-001.py
+++ Exercicio-001.py
@@ -16,28 +16,8 @@ x = np.linspace(-2, 3, 400)
 y1 = eq1(x)
 y2 = eq2(x)
 
-
 plt.plot(x, y1, label=r'$y = 0.5x + 0.5$', color='blue')
 plt.plot(x, y2, label=r'$y = -x + 2$', color='red')
 
-
 plt.scatter(1, 1, color='green', zorder=5)
 plt.text(1, 1, '(1, 1)', fontsize=12, verticalalignment='bottom')
-
-
-plt.xlim(-2, 3)
-plt.ylim(-2, 3)
-
-
-plt.title("Solução do sistema de equações lineares")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
-plt.grid(True)
-
-
-plt.legend()
-
-
-plt.show()
